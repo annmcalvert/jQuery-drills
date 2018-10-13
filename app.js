265c6a63e2657301a9ff1366c2e5a3fe8cf4615c
annmcalvert <annmcalvert@gmail.com>
2018-10-13 13:30:38 -0500

    working on li changing color

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
 $(document).ready(function () {
 
+    //creates a div and ul
+    let div = document.createElement('div');
+    let ul = document.createElement('ul');
+    $('body').append(div);
+    $('div').append(ul);
 
-    //gives alert when button is clicked
+    //gives li when button is clicked
     $('#btnSubmit').click(function () {
-        // let alertValue = $('#textInput').val();
-        // alert(alertValue);
         let text = $('#textInput').val();
-        $('h2').append(text);
-
+        // $('h2').append(text);
+        let li = document.createElement('li');
+        $(li).append(text);
+        $('ul').append(li);
+        return false;
     });
 
+    //if input text is > 0, the submit button is no longer disabled
     //calls action function when keyup on input box
     $('#textInput').on('keyup', action);
-
-    //if input text is > 0, the submit button is no longer disabled
     function action() {
         if ($('#textInput').val().length > 0) {
             $('#btnSubmit').removeAttr('disabled');
         }
-    }
+    };
 
-    let div = document.createElement('div');
-    let h2 = document.createElement('h2');
-    $('body').append(div);
-    $('div').append(h2);
+    //changes color of li
+    //not working yet
+    $('li').click(function () {
+        let colors = ["blue", "green", "yellow", "orange", "red", "purple"];
+        let randomColor = () => colors[Math.ceil(Math.random() * colors.length - 1)];
+        this.style.color = randomColor();
 
+    })
+
+
+
+
+    //hovering
+    //not needed
+    // $('h2').hover(function() {
+    //     $(this).css({"background-color": "blue", "border-radius": "5px"});
+    // }, function() {
+    //     $(this).css({"background-color": "", "border-radius": ""});
+    // });
 
 });
